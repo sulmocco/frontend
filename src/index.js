4c937018890204a532161bfa6c38bd5274fec4b4
imhjnoh <imhjnoh@gmail.com>
2022-08-06 21:18:55 +0900

    Init: project initial settings

--- src/index.js
+++ src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import store from "./redux/store";
+
+const queryClient = new QueryClient()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </BrowserRouter>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/redux/userSlice.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const initialState = {
+  isLogin: false,
+  NAME: ""
+};
+
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    userLogin(state, action) {
+      state.isLogin = true
+      state.NAME = action.payload
+    },
+    userLogout(state, action) {
+      state.isLogin = false;
+      state.NAME = '';
+      localStorage.removeItem('token');
+    }
+  },
+  extraReducers: {}
+});
+
+export default userSlice.reducer;
+export const userActions = userSlice.actions;

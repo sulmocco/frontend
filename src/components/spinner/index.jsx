2f6435fa8ba4a7371f078003e6fb5023927d50d8
102 <89123240+102wy@users.noreply.github.com>
2022-08-11 13:39:58 +0900

    CREATE: create spinner

---
+++ src/components/spinner/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SpinnerWrap } from './styles';
+
+const Spinner = () => {
+    return (
+        <SpinnerWrap>
+            <span></span>
+        </SpinnerWrap>
+    );
+};
+
+export default Spinner;

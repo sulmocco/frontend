deccc84da02fc9c102951e2aca3ceb92bd27519e
imhjnoh <imhjnoh@gmail.com>
2022-08-11 10:59:39 +0900

    FIX: merge issues

--- src/components/recommend/index.jsx
+++ src/components/recommend/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import { AlchholTag, SnackTag } from '../../styles/CommonStyles';
 import { RecommendItem } from './styles';
 

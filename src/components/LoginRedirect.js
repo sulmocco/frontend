676a6ecf185d1eb94eae7747822be7d64b05f4da
102 <89123240+102wy@users.noreply.github.com>
2022-08-13 20:38:11 +0900

    UPDATE: path userId

--- src/components/LoginRedirect.js
+++ src/components/LoginRedirect.js
@@ -16,8 +16,8 @@ const LoginRedirect = () => {
       const res = await axios.get(
         `http://13.209.8.162/oauth2/redirect?code=${queryData.code}`
       );
-      console.log("리스폰스를 주세요!!", res);
-      navigate("/signup");
+      console.log("데이터확인", res.data);
+      navigate(`/signup?userId=${res.data.userId}`);
     } catch (err) {
       alert("성인이 아닙니다!");
       navigate("/loginrending");

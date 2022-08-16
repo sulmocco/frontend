7217c5079817a3ffc9f243871d85278dfa592ba6
imhjnoh <imhjnoh@gmail.com>
2022-08-16 14:43:06 +0900

    CREATE: placeholders, flex to grid

--- src/pages/tables/styles.jsx
+++ src/pages/tables/styles.jsx
@@ -2,8 +2,12 @@ import styled from "styled-components";
 
 export const TablesWrapper = styled.div`
     width: 100%;
+    max-width: 100vw;
     padding: 10rem 0;
-    display: flex;
-    flex-wrap: wrap;
-    gap: 3.2rem;
+    display: grid;
+    /* flex-wrap: wrap; */
+    /* grid-template-columns: 1fr 1fr 1fr; */
+    /* gap: 3.2rem; */
+    grid-template-columns: repeat(auto-fit, minmax(40.5rem, 1fr));
+    grid-gap: 3.2rem;
 `

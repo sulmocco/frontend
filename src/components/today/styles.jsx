bfabc40e436690f6d6d6391b4e1807ec821c7bb4
imhjnoh <imhjnoh@gmail.com>
2022-08-21 17:16:35 +0900

    FIX: show level in main/today

--- src/components/today/styles.jsx
+++ src/components/today/styles.jsx
@@ -27,76 +27,80 @@ export const Container = styled.div`
 export const RankWrapper = styled.div`
   display: flex;
   justify-content: center;
-`
+`;
 export const OneRankWrapper = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: flex-end;
-&:first-child{
-  margin-right: 3.2rem;
-}
-&:last-child{
-  margin-left: 3.2rem;
-}
-  .crown{
-    margin-bottom: .4rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-end;
+  transition: all 0.2s ease-in-out;
+  &:hover {
+    transform: translateY(-0.5rem);
+  }
+  &:first-child {
+    margin-right: 3.2rem;
+  }
+  &:last-child {
+    margin-left: 3.2rem;
+  }
+  .crown {
+    margin-bottom: 0.4rem;
     width: inherit;
     justify-content: center;
-    img{
-    width: ${props => props.first ? 8.9 : 6.3}rem;
+    img {
+      width: ${(props) => (props.first ? 8.9 : 6.3)}rem;
     }
   }
-`
+`;
 export const TableCardWrapper = styled.div`
   border-radius: 1rem;
   background-color: white;
   width: 34.4rem;
-  box-shadow: ${props => props.theme.shadow_gray};
+  box-shadow: ${(props) => props.theme.shadow_gray};
   display: flex;
   flex-direction: column;
   overflow: hidden;
   /* align-items: center; */
-  .img{
+  .img {
     background-color: azure;
-    height: ${props => props.first ? 20.8 : 16.8}rem;
-    background-image: ${props => "url("+props.src+")"};
+    height: ${(props) => (props.first ? 20.8 : 16.8)}rem;
+    background-image: ${(props) => "url(" + props.src + ")"};
     background-size: cover;
     background-position: center;
   }
-  .bottom{
+  .bottom {
     padding: 2.4rem 3.2rem;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
   }
-  .profile{
+  .profile {
     width: 6.4rem;
     height: 6.4rem;
     border-radius: 6.4rem;
-    background-image: ${props => "url("+props.profile+")"};
+    background-image: ${(props) => "url(" + props.profile + ")"};
     background-size: cover;
     background-position: center;
     background-color: aqua;
   }
-  .counterWrap{
+  .counterWrap {
     width: 100%;
     height: 2.4rem;
-    margin-top: .8rem;
+    margin-top: 0.8rem;
     display: flex;
     flex-direction: row;
     font-size: 1.6rem;
     justify-content: center;
     align-items: center;
     font-weight: 500;
-    color: ${props => props.theme.grey_02};
-    gap: .4rem;
-    img{
+    color: ${(props) => props.theme.grey_02};
+    gap: 0.4rem;
+    img {
       width: 1.8rem;
     }
   }
-  .title{
+  .title {
     width: 100%;
     font-size: 2.6rem;
     font-weight: 700;
@@ -106,30 +110,30 @@ export const TableCardWrapper = styled.div`
     white-space: nowrap;
     text-overflow: ellipsis;
   }
-  .username{
+  .username {
     width: 100%;
-    color: ${props => props.theme.primary};
+    color: ${(props) => props.theme.primary};
     overflow: hidden;
     white-space: nowrap;
     text-overflow: ellipsis;
     font-weight: 500;
     font-size: 2rem;
     text-align: center;
-    margin-top: .4rem;
+    margin-top: 0.4rem;
     display: flex;
     flex-direction: row;
     justify-content: center;
     align-items: center;
-    gap: .6rem;
+    gap: 0.6rem;
   }
-  .alcohol{
-    padding: .4rem 1.2rem;
+  .alcohol {
+    padding: 0.4rem 1.2rem;
     border-radius: 2rem;
-    background-color: ${props => props.theme.light_yellow_01};
+    background-color: ${(props) => props.theme.light_yellow_01};
   }
-  .tags{
+  .tags {
     margin-top: 1.8rem;
     display: flex;
-    gap: .4rem;
+    gap: 0.4rem;
   }
-`
+`;

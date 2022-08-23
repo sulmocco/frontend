fea15d86d20aeda13ca81ff51403904717f2334a
imhjnoh <imhjnoh@gmail.com>
2022-08-23 21:08:25 +0900

    FIX: chatting

--- src/pages/chat/index.jsx
+++ src/pages/chat/index.jsx
@@ -7,9 +7,9 @@ import { ChatWrap } from './styles';
 
 const Chat = () => {
     const [message, setMessage] = useState('');
-    const [content, setContent] = useState('');
+    const [content, setContent] = useState([]);
     const chat_ref = useRef();
-    const { roomId } = useParams();
+    const { chatRoomId } = useParams();
     const token = localStorage.getItem('token');
     const username = localStorage.getItem('username');
 
@@ -20,17 +20,27 @@ const Chat = () => {
     const headers = { Authorization: token };
 
     // 웹소켓 연결시 stomp에서 자동으로 connect이 되었다는것을 console에 보여주는데 그것을 감추기 위한 debug
-    client.debug = null;
+    // client.debug = null;
 
     // 소켓연결
     const socketConnect = () => {
         try {
             client.connect(headers, () => {
-                const res = client.subscribe(`/sub/chat/room/${roomId}`,headers, (data) => {
+                const res = client.subscribe(`/sub/chat/room/${chatRoomId}`, (data) => {
+                    console.log("데이터라도 보여줘: ", data);
                     const newMessage = JSON.parse(data.body);
+                    console.log("--- 메시지 내용 ---");
                     console.log(data.body);
-                    setContent(newMessage);
-                });
+                    if(content.length>0){
+                    const newContent = [...content]
+                    newContent.push(newMessage.message)
+                    console.log([...newContent]);
+                    setContent(newContent);
+                    }else{
+                        setContent([newMessage.message])
+                        console.log([newMessage.message]);
+                    }
+                },headers);
                 console.log(res);
             });
         }
@@ -50,16 +60,40 @@ const Chat = () => {
         }
     }
 
+    // const quitChatroom = async() => {
+    //     const res = await client.send(`pub/chat/message`, headers, JSON.stringify({
+    //         type: 'QUIT',
+    //         chatRoomId: chatRoomId,
+    //         sender: username,
+    //         message: chat_ref.current.value
+    //     }))
+    // }
+
+    const enterChatroom = async() => {
+        const res = await client.send(`pub/chat/message`, headers, JSON.stringify({
+            type: 'ENTER',
+            chatRoomId: chatRoomId,
+            sender: username,
+            message: chat_ref.current.value
+        }))
+    }
+
     // 메세지 보내기
     const sendMessage = async () => {
         // try {
-            const res = await client.send(`/pub/chat/message`, JSON.stringify({
+            console.log({
                 type: 'TALK',
-                chatRoomId: roomId,
+                chatRoomId: chatRoomId,
+                sender: username,
+                message: chat_ref.current.value
+            });
+            const res = await client.send(`/pub/chat/message`,headers, JSON.stringify({
+                type: 'TALK',
+                chatRoomId: chatRoomId,
                 sender: username,
                 message: chat_ref.current.value
             }))
-            console.log("과연"+ res);
+            console.log("SEND가 끝남. res : "+ res);
             // if (chat_ref === '') {
             //     return
             // }
@@ -75,20 +109,35 @@ const Chat = () => {
     //roomId가 바뀔때마다 다시 연결
     useEffect(() => {
         socketConnect();
-        sulmoggoApi.enterChatRoom(roomId)
-        sulmoggoApi.getRoomData(roomId)
+        try{
+        sulmoggoApi.enterChatRoom(chatRoomId)
+        sulmoggoApi.getRoomData(chatRoomId)}
+        catch{
+            console.log("뭔가 잘못됨");
+        }
+        // return (() => {
+        //     socketDisConnect()
+        // })
     }, [])
     return (
         <ChatWrap>
             <div className="message-wrap">
-                <span>{content}</span>
+               {content.map(data => {
+                return <div style={{display: "block"}}>--*{data}*--</div>
+               })}
             </div>
             <form action="">
                 <input type="text" placeholder='채팅치시오' ref={chat_ref} />
                 <button onClick={(e) => {
                     e.preventDefault();
                     sendMessage(chat_ref.current.value);
+                    console.log("보내기버튼. 내용 : ", chat_ref.current.value);
                 }}>보내기</button>
+                {/* <button onClick={(e) => {
+                    e.preventDefault();
+                    quitChatroom();
+                    console.log("나가기버튼. 내용 : ", chat_ref.current.value);
+                }}>나가기</button> */}
             </form>
         </ChatWrap>
     );

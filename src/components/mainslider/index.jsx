14a0489d30eb7d61d59a93fdcc5688611793cda9
102 <89123240+102wy@users.noreply.github.com>
2022-09-01 13:58:24 +0900

    FIX: 아이콘 변경

--- src/components/mainslider/index.jsx
+++ src/components/mainslider/index.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Autoplay, Pagination } from "swiper";
+import { Navigation, Autoplay, Pagination, Controller } from "swiper";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
@@ -9,17 +9,21 @@ import {
 } from "./styles";
 
 const MainSlider = () => {
+  const [isAuto, setAuto] = useState(true);
+  const pause = document.querySelector('.swiper-pagination');
+  window.onload = function () {
+    pause.addEventListener('click', () => alert('눌러짐'));
+  }
   return (
     <Carousel>
       <Swiper
-        modules={[Navigation, Pagination, Autoplay]}
+        modules={[Navigation, Pagination, Autoplay, Controller]}
         spaceBetween={50}
         slidesPerView={1}
-        // onSlideChange={() => console.log("slide change")}
         navigation
-        pagination={{ clickable: true }}
+        pagination={{ clickable: true, type: 'fraction' }}
         loop={true}
-        autoplay={true}
+        autoplay={isAuto}
       >
         <SwiperSlide>
           <Image src="/images/banner-1.png" alt="" />
--- src/components/mainslider/styles.jsx
+++ src/components/mainslider/styles.jsx
@@ -1,22 +1,58 @@
 import styled from "styled-components";
 
 export const Carousel = styled.section`
-  /* margin-top: 30px; */
+  .swiper{
+    position: relative;
+    display: flex;
+  }
   .swiper-button-prev,
   .swiper-button-next {
+    width: 2.1rem;
+    height: 5.2rem;
     &::after {
-      background: rgba(0, 0, 0, 0.15);
-      border-radius: 50%;
-      color: #fff;
-      font-size: 17px;
-      padding: 11px 16px;
+      display: none;
     }
   }
   .swiper-button-prev {
-    margin-left: 25px;
+    margin-left: 30rem;
+    background-image:url('/images/icon_arrow_left.svg');
+    background-repeat: no-repeat;
+    background-size: contain;
   }
   .swiper-button-next {
-    margin-right: 25px;
+    margin-right: 30rem;
+    background-image:url('/images/icon_arrow_right.svg');
+    background-repeat: no-repeat;
+    background-size: contain;
+  }
+  .swiper-pagination {
+    position: absolute;
+    left: 135.8rem;
+    bottom: 4rem;
+    display: flex;
+    align-items: center;
+    width: 11.7rem;
+    height: 3.4rem;
+    color: #fff;
+    font-size: 1.6rem;
+    background-color: #4e4e56;
+    border-radius: 2.4rem;
+    letter-spacing: -0.04rem;
+    &::before {
+      content: '';
+      display: block;
+      background-image: url('/images/icon_pause.svg');
+      width: .8rem;
+      height: 1.4rem;
+      margin: 0 1.9rem 0 1.7rem;
+      cursor: pointer;
+    }
+    span {
+      padding: 0 .8rem;
+    }
+    .swiper-pagination-total {
+      color: ${props => props.theme.grey_03};
+    }
   }
 `;
 

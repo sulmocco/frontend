062f670a0743f1cb8019b0207e3038c728ffe9a0
102 <89123240+102wy@users.noreply.github.com>
2022-09-01 17:51:50 +0900

    FIX: recommend 컴포넌트 분리

--- src/components/recommend/index.jsx
+++ src/components/recommend/index.jsx
@@ -1,18 +1,29 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import sulmoggoApi from '../../shared/apis';
 import { AlchholTag, SnackTag } from '../../styles/CommonStyles';
-import { RecommendItem } from './styles';
+import { RecommendItem, RecommendWrap } from './styles';
 
-const Recommend = ({ url, img, title, price, tag }) => {
+const Recommend = () => {
+    const { data } = useQuery(["products"], () =>
+        sulmoggoApi.getProducts().then((res) => res.data));
+    console.log(data);
     return (
-        <RecommendItem onClick={() => window.open(`${url}`, '_blank')}>
-            <img src={img} alt='상품이미지' />
-            <p className='title'>{title}</p>
-            <span className='price'>{price} <p>원</p></span>
-            <div className='tag'>
-                <AlchholTag >{tag}</AlchholTag>
-                <SnackTag >{tag}</SnackTag>
-            </div>
-        </RecommendItem>
+        <RecommendWrap>
+            <h2>이상품 어때요?</h2>
+            <section className="recommendsection">
+                {data?.map((item, index) => (
+                    <RecommendItem key={item.id} onClick={() => window.open(`${item.redirectUrl}`, '_blank')}>
+                        <img src={item.imageUrl} alt='상품이미지' />
+                        <p className='title'>{item.name}</p>
+                        <span className='price'>{item.price} <p>원</p></span>
+                        <div className='tag'>
+                            <AlchholTag >{item.alcoholtag}</AlchholTag>
+                        </div>
+                    </RecommendItem>
+                ))}
+            </section>
+        </RecommendWrap>
     );
 };
 

31d115a8fd5647c365b20baca7d1a6691f17e5b6
102 <89123240+102wy@users.noreply.github.com>
2022-09-06 01:07:34 +0900

    CREATE: 탈퇴전 확인페이지 추가

--- src/pages/liverending/styles.jsx
+++ src/pages/liverending/styles.jsx
@@ -17,6 +17,14 @@ export const LiveRendingCont = styled.div`
     display: flex;
     align-items: center;
     flex-direction: column;
+    p {
+        font-size: 2.6rem;
+        font-weight: 400;
+        line-height: 3.1rem;
+        letter-spacing: -0.02rem;
+        margin-top: 1.6rem;
+        color: ${props => props.theme.grey_02};
+    }
     h3{
         font-size: 3.2rem;
         line-height: 3.819rem;
